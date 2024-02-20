ea18164e8e2385661afca46a46f5c2fa8a203126
Javedmv <javedmv777@gmail.com>
2024-02-21 05:20:46 +0530

    added tweet quote

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // Get quotes from API
+const quoteContainer = document.getElementById("qoute-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 function newQuote(){
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+
+    if(quote.text.length > 50){
+        quoteText.classList.add("long-quote")
+    }else{
+        quoteText.classList.remove("long-quote")
+    }
+    quoteText.textContent = quote.text;
 }
 
     async function getQuotes(){
@@ -16,4 +33,14 @@ function newQuote(){
             
         }
     };
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+};
+
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
+
     getQuotes();
